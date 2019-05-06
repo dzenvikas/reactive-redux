4428166e120324fa8132b40317b260284b23fd73
dzenvikas <vikaskumar.vsk@gmail.com>
2019-05-07 00:15:32 +0530

    UI update

--- src/components/CounterOutput/CounterOutput.js
+++ src/components/CounterOutput/CounterOutput.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './CounterOutput.css';
 
-const counterOutput = (props) => (
-    <div className="CounterOutput">
-        Current Counter: {props.value}
+const counterOutput = props => (
+  <div className="CounterOutput">
+    <div className="container">
+      <label>Current Counter:</label>
+      <h1>{props.value}</h1>
     </div>
+  </div>
 );
 
 export default counterOutput;
