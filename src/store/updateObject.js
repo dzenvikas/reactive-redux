59827f2fa0c8d2d3547165af365cb7086c5e2ff1
dzenvikas <vikaskumar.vsk@gmail.com>
2019-05-07 12:36:05 +0530

    Typo fix

--- src/store/updateObject.js
+++ src/store/updateObject.js
@@ -1,6 +1,6 @@
-export const updateObject = (oldObject, updatedObject) {
-    return {
-        ...oldObject,
-        ...updatedObject
-    }
-}
+export const updateObject = (oldObject, updatedObject) => {
+  return {
+    ...oldObject,
+    ...updatedObject
+  };
+};
